887e2c946941d4bbe67cd3b46cfd2bc7de17907a
hahagarden <tingyuan413@gmail.com>
2023-04-03 12:51:26 +0900

    feat: Nav and Feature components and router

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,3 +1,20 @@
-function Feature() {}
+import { Routes, Route } from "react-router-dom";
+import Chats from "../pages/Chats";
+import Chat from "../pages/Chat";
+import ScrapBooks from "../pages/ScrapBooks";
+import Scraps from "../pages/Scraps";
+
+function Feature() {
+  return (
+    <section className="feature">
+      <Routes>
+        <Route path="/" element={<Chats />} />
+        <Route path="/chats/:chatId" element={<Chat />} />
+        <Route path="/scrapbooks" element={<ScrapBooks />} />
+        <Route path="/scrapbooks/:scrapbookId" element={<Scraps />} />
+      </Routes>
+    </section>
+  );
+}
 
 export default Feature;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,3 +1,22 @@
-function Nav() {}
+import { NavLink } from "react-router-dom";
+
+function Nav() {
+  return (
+    <section className="nav">
+      <NavLink to="/" className="navItem">
+        <i className="navIcon fa-solid fa-mug-hot"></i>
+        <div className="navText">Chat</div>
+      </NavLink>
+      <NavLink to="/scrapbooks" className="navItem">
+        <i className="navIcon fa-regular fa-bookmark"></i>
+        <div className="navText">Scrap</div>
+      </NavLink>
+      <NavLink to="/highlights" className="navItem">
+        <i className="navIcon fa-solid fa-highlighter"></i>
+        <div className="navText">Highlight</div>
+      </NavLink>
+    </section>
+  );
+}
 
 export default Nav;
