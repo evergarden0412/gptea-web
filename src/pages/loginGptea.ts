2efec9257e58a0c7a375e79d8d86ddd432c057f1
hahagarden <tingyuan413@gmail.com>
2023-05-12 18:18:12 +0900

    fix: fix loginGptea promise logic

--- src/pages/loginGptea.ts
+++ src/pages/loginGptea.ts
@@ -7,8 +7,8 @@ const registerInGptea = (accessToken: string, social: string): Promise<void> => 
   return new Promise((resolve, reject) => {
     axios('/auth/cred/register', { method: 'POST', data: { accessToken, cred: social } })
       .then(() => {
+        console.log('registering');
         resolve();
-        getGpteaToken(accessToken, social);
       })
       .catch((error) => {
         console.log({ ERROR_REGISTER_IN_GPTEA, error });
@@ -25,12 +25,17 @@ export const getGpteaToken = (accessToken: string, social: string): Promise<void
         if (!localStorage.getItem(GPTEA_ACCESS_TOKEN)) {
           localStorage.setItem(GPTEA_ACCESS_TOKEN, accessToken);
         }
+        console.log('getting access token');
         resolve();
         // setIsLoggedIn(true);
       })
       .catch((err) => {
         console.log({ ERROR_GET_GPTEA_TOKENS, err });
-        registerInGptea(accessToken, social).catch(() => reject(ERROR_GET_GPTEA_TOKENS));
+        registerInGptea(accessToken, social)
+          .then(() => {
+            getGpteaToken(accessToken, social).then(() => resolve());
+          })
+          .catch(() => reject(ERROR_GET_GPTEA_TOKENS));
       });
   });
 };
