697d603c2cb0f37d37688e28584d1d456b14131c
hahagarden <tingyuan413@gmail.com>
2023-05-23 21:36:08 +0900

    feat: add maintain-login-when-refresh feat

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import styled from 'styled-components';
 import { Routes, Route } from 'react-router-dom';
-import { useAppSelector } from './redux/hooks';
 
 import MyGptea from './pages/MyGptea';
 import Login from './pages/Login';
 import NaverLogin from './pages/NaverLogin';
 import KakaoLogin from './pages/KakaoLogin';
+import { GPTEA_ACCESS_TOKEN } from './utils/loginGpteaFunc';
+import { useAppSelector, useAppDispatch } from './redux/hooks';
+import { login } from './redux/isLoggedInSlice';
+import { useEffect } from 'react';
 
 const AppWrapper = styled.div`
   width: 80vw;
@@ -13,6 +16,14 @@ const AppWrapper = styled.div`
 `;
 
 function App() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (localStorage.getItem(GPTEA_ACCESS_TOKEN)) {
+      dispatch(login());
+    }
+  }, []);
+
   const isLoggedIn = useAppSelector((state) => state.isLoggedIn);
 
   return (
