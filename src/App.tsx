919a5175f46099877ddf89c0811e9fb328700ac0
hahagarden <tingyuan413@gmail.com>
2023-06-26 15:39:33 -0700

    design: remove naver-login-feat and fix login css

--- src/App.tsx
+++ src/App.tsx
@@ -21,10 +21,6 @@ import { recreateGpteaToken } from "./api/gpteaAuth";
 const AppWrapper = styled.div`
   width: 100vw;
   height: 100vh;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
 `;
 
 const StyledToastContainer = styled(ToastContainer)`
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -20,7 +20,6 @@ const kakaoLogin = window.Kakao;
 
 function Login() {
   useEffect(() => {
-    naverLogin.init();
     if (!kakaoLogin.isInitialized()) kakaoLogin.init(process.env.REACT_APP_KAKAO_JAVASCRIPT_KEY);
   }, []);
 
@@ -47,10 +46,9 @@ function Login() {
         </Discription>
       </DiscriptionContainer>
       <ButtonContainer>
-        <div id="naverIdLogin" />
-        <div id="kakaoIdLogin" onClick={handleKakaoLogin}>
+        <Button id="kakaoIdLogin" onClick={handleKakaoLogin}>
           <img src={kakaoLoginLogo} />
-        </div>
+        </Button>
       </ButtonContainer>
       <Svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
         <path d="M88 0C74.7 0 64 10.7 64 24c0 38.9 23.4 59.4 39.1 73.1l1.1 1C120.5 112.3 128 119.9 128 136c0 13.3 10.7 24 24 24s24-10.7 24-24c0-38.9-23.4-59.4-39.1-73.1l-1.1-1C119.5 47.7 112 40.1 112 24c0-13.3-10.7-24-24-24zM32 192c-17.7 0-32 14.3-32 32V416c0 53 43 96 96 96H288c53 0 96-43 96-96h16c61.9 0 112-50.1 112-112s-50.1-112-112-112H352 32zm352 64h16c26.5 0 48 21.5 48 48s-21.5 48-48 48H384V256zM224 24c0-13.3-10.7-24-24-24s-24 10.7-24 24c0 38.9 23.4 59.4 39.1 73.1l1.1 1C232.5 112.3 240 119.9 240 136c0 13.3 10.7 24 24 24s24-10.7 24-24c0-38.9-23.4-59.4-39.1-73.1l-1.1-1C231.5 47.7 224 40.1 224 24z" />
@@ -84,6 +82,9 @@ const DiscriptionContainer = styled.div`
 
 const ImageContainer = styled.div`
   z-index: 1;
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: flex-start;
 `;
 
 const Image = styled.img`
@@ -116,4 +117,8 @@ const ButtonContainer = styled.div`
   width: 450px;
   justify-content: space-between;
   z-index: 1;
+`;
+
+const Button = styled.div`
+  cursor: pointer;
 `;
